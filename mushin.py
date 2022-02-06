6dc296818cf46208feb3b200d783a7dcb09ef1dc
Freddy Jilson <jilsonfreddy@hotmail.com>
2022-02-07 04:18:37 +0800

    Add cross-platform desktop app with file selector for AES-256 encryption.

--- mushin.py
+++ mushin.py
@@ -1,16 +1,31 @@
+import os
+import re
+
 import dearpygui.dearpygui as dpg
+
 
 dpg.create_context()
 
-def callback(sender, app_data, user_data):
-    print("Sender: ", sender)
-    print("App Data: ", app_data)
 
-with dpg.file_dialog(directory_selector=False, show=False, callback=callback, id="file_dialog_id"):
+def callback(sender, app_data, user_data):
+    file_path = re.search(r"(?:'selections': )(.*)", str(app_data)).group(1)
+    file_path = re.search(r"(?:: )(.*)", str(file_path)).group(1)
+    file_path = file_path.replace("'", "")
+    file_path = file_path.replace("}", "")
+
+    if os.path.exists(file_path):
+        file_name_with_ext = str(os.path.basename(file_path))
+        file_name_without_ext, dot, ext = file_name_with_ext.partition('.')
+
+    print(os.path.dirname(file_path))
+
+
+with dpg.file_dialog(directory_selector=False, show=False, callback=callback, id="file_dialog_id", width=500,
+                     height=500):
     dpg.add_file_extension(".*")
 
 with dpg.window(label="Select file to encrypt", width=800, height=600):
-    dpg.add_button(label="Select file", callback=lambda: dpg.show_item("file_dialog_id"))
+    dpg.add_button(label="Select file", width=800, height=100, callback=lambda: dpg.show_item("file_dialog_id"))
 
 dpg.create_viewport(title='Mushin', width=800, height=600)
 dpg.setup_dearpygui()
