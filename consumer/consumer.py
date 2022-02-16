1e350fd4bdb3c9dc7ad96ea6e4add2146448f5fc
Freddy Jilson <jilsonfreddy@hotmail.com>
2022-02-16 20:28:48 +0800

    Dockerize encryptor and consumer queues

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,14 +1,15 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.dirname(__file__))
 
 import pika
-from aes.aes import encrypt_file
-
+from encryptor.aes import encrypt_file
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq3', credentials=pika.credentials.PlainCredentials("mushin_user", "mushin_pass")))
+    pika.ConnectionParameters(host='rabbitmq3',
+                              credentials=pika.credentials.PlainCredentials("mushin_user", "mushin_pass")))
 channel = connection.channel()
 
 channel.queue_declare(queue='encryption_uploads', durable=True)
--- docker/consumer.Dockerfile
+++ docker/consumer.Dockerfile
@@ -3,10 +3,11 @@ FROM python:3.8-slim
 WORKDIR /app
 
 RUN /usr/local/bin/python -m pip install --upgrade pip && \
-    pip3 install pika &&
+    pip3 install pika && \
+    pip3 install pycryptodome
 
 COPY ./consumer /app/consumer
-COPY ./aes /app/aes
+COPY ./encryptor /app/encryptor
 
 ENTRYPOINT [ "python3" ]
 CMD [ "consumer/consumer.py" ]
